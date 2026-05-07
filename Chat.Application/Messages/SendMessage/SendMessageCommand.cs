a2ab03c380443bf001fb340065a3391a547fe08b
DASKALELIS_Emmanouil <Manolisdaskalelis@gmail.com>
2026-05-07 15:04:10 +0300

    Messaging in progress

---
+++ Chat.Application/Messages/SendMessage/SendMessageCommand.cs
@@ -0,0 +1,12 @@
+﻿using Chat.Contracts.Messages;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Application.Messages.SendMessage
+{
+    public sealed record SendMessageCommand(Guid RoomId, string Content): IRequest<MessageDto>;
+}
---
+++ Chat.Contracts/Messages/MessageDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Contracts.Messages
+{
+    public sealed record MessageDto(Guid Id, Guid RoomId, Guid SenderId, string Content, string Type, DateTime CreatedAtUtc);
+}
